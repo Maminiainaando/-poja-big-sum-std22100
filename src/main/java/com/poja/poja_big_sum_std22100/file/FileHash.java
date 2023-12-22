6bc70303ec2405977d74827878f7d77e1d839865
Maminiainaando <hei.maminiaina.444@gmail.com>
2023-12-22 09:10:34 +0300

    project bigsum poja

---
+++ src/main/java/com/poja/poja_big_sum_std22100/file/FileHash.java
@@ -0,0 +1,6 @@
+package com.poja.poja_big_sum_std22100.file;
+
+import com.poja.poja_big_sum_std22100.PojaGenerated;
+
+@PojaGenerated
+public record FileHash(FileHashAlgorithm algorithm, String value) {}
